37bef7f7697b3108be4513aba4731acebb78102f
AlyAbdelrahman <aly.m.abdelrahman@gmail.com>
2026-03-24 22:44:26 +0200

    Refactor styles and layout for task management dashboard
    - Updated Angular configuration to use new SCSS structure with main.scss.
    - Replaced styles.scss with a new structure for better organization.
    - Enhanced header component with Material Design elements and improved layout.
    - Added new SCSS files for components, abstracts, and themes to support modular styling.
    - Introduced 'concurrently' as a dependency for running multiple tasks simultaneously.

--- task-management-dashboard/src/app/layout/components/header/header.ts
+++ task-management-dashboard/src/app/layout/components/header/header.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatFormField, MatPrefix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbar, MatFormField, MatInput, MatPrefix, MatIcon, MatIconButton],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
--- task-management-dashboard/src/app/layout/layout.component.ts
+++ task-management-dashboard/src/app/layout/layout.component.ts
@@ -4,6 +4,7 @@ import { Sidebar } from './components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [Header, Sidebar, RouterOutlet],
   template: `
     <app-header></app-header>
